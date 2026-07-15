d2e15f549056c6cdda69986b8ce135d6e63b946a
Emmanuel Mugaine <murithi030.gmail.com>
2026-07-15 19:03:11 +0100

    Edited file reading blocks of code to store accurate data types for the values in the dictionaries

--- Personal_Finance_Manager.py
+++ Personal_Finance_Manager.py
@@ -2,22 +2,54 @@ import csv  #For dictreader and dictwriter
 from datetime import date, datetime #Converting string date values into actual dates
 
 #--------- Opening and storing the Accounts csv file ------------
-with open("accounts.csv" , "r", newline= "") as accounts_file:
-    account_dict = csv.DictReader(accounts_file)
-    for row in account_dict:
-        print(row)
+accounts_list = []
+def load_accounts(filename):    #Reusable function to open any account file
+    with open(filename , "r", newline= "") as accounts:
+        account_reader = csv.DictReader(accounts)   #Storing file values as dictionaries
+        for row in account_reader:
+            accounts_list.append({
+                "Account ID" : row["Account ID"],
+                "Account Name" : row["Account Name"]
+            })
 
+load_accounts("accounts.csv")
+
+budgets_list = []
 #--------- Opening and storing the Budget csv file -------------
-with open("budget.csv" , "r", newline= "") as budget_file:
-    budget_dict = csv.DictReader(budget_file)
+def load_budget(filename):
+    with open(filename , "r", newline= "") as budget:
+        budget_reader = csv.DictReader(budget)
+    #Storing and Editing values to their appropriate data types
+        for row in budget_reader:
+            budgets_list.append({
+                "Account ID" : row["Account ID"],
+                "Category" : row["Category"],
+                "Budget Amount" : float(row["Budget Amount"])
+            })
+#Loading the budgets.csv file
+load_budget("budgets.csv")
 
+transactions_list = []
 #--------- Opening and storing the transactions csv file --------------
-with open("transactions.csv" , "r", newline= "") as transactions_file:
-    transaction_dict = csv.DictReader(transactions_file)
-    #Converting string dates into actual dates
-    for row in transaction_dict:
-        d = date.fromisoformat(row["Date"])
-        #print(d)
+def load_transactions(filename):
+    #Opening
+    with open(filename , "r", newline= "") as transactions:
+        transaction_reader = csv.DictReader(transactions)
+    #Storing and Editing values to their appropriate data types
+        for row in transaction_reader:  #Iterating the values in the reader
+            transactions_list.append({ #Storing the values into the empty list created outside
+                "Account ID" : row["Account ID"],
+                "Date" : datetime.strptime( #Formatting date into YYYY-MM-DD
+                row["Date"],
+                "%Y-%m-%d"
+                ).date(),
+                "Company Name" : row["Company Name"],
+                "Amount" : float(row["Amount"]),    #Formatting date into floats
+                "Category" : row["Category"],
+                "Description" : row["Description"]
+            })
+load_transactions("transactions.csv")
+print(transactions_list[0])
+print
 
-    #Printing only a specific field
-        #print(row)
+
--- test.py
+++ test.py
@@ -1,2 +1,55 @@
-accounts = {"Account ID" : "00001", "Account Name" : "Emmanuel Mugaine"}
-transactions = {"Account ID" : "00001", "Date" : "26-07-2026", "Amount" : -26.70, "Company name": "Aldi", "Category" : "Groceries"}
+import csv  #For dictreader and dictwriter
+from datetime import date, datetime #Converting string date values into actual dates
+
+#--------- Opening and storing the Accounts csv file ------------
+accounts_list = []
+def load_accounts(filename):    #Reusable function to open any account file
+    with open(filename , "r", newline= "") as accounts:
+        account_reader = csv.DictReader(accounts)   #Storing file values as dictionaries
+        for row in account_reader:
+            accounts_list.append({
+                "Account ID" : row["Account ID"],
+                "Account Name" : row["Account Name"]
+            })
+
+load_accounts("accounts.csv")
+
+budgets_list = []
+#--------- Opening and storing the Budget csv file -------------
+def load_budget(filename):
+    with open(filename , "r", newline= "") as budget:
+        budget_reader = csv.DictReader(budget)
+    #Storing and Editing values to their appropriate data types
+        for row in budget_reader:
+            budgets_list.append({
+                "Account ID" : row["Account ID"],
+                "Category" : row["Category"],
+                "Budget Amount" : float(row["Budget Amount"])
+            })
+#Loading the budgets.csv file
+load_budget("budgets.csv")
+
+transactions_list = []
+#--------- Opening and storing the transactions csv file --------------
+def load_transactions(filename):
+    #Opening
+    with open(filename , "r", newline= "") as transactions:
+        transaction_reader = csv.DictReader(transactions)
+    #Storing and Editing values to their appropriate data types
+        for row in transaction_reader:  #Iterating the values in the reader
+            transactions_list.append({ #Storing the values into the empty list created outside
+                "Account ID" : row["Account ID"],
+                "Date" : datetime.strptime( #Formatting date into YYYY-MM-DD
+                row["Date"],
+                "%Y-%m-%d"
+                ).date(),
+                "Company Name" : row["Company Name"],
+                "Amount" : float(row["Amount"]),    #Formatting date into floats
+                "Category" : row["Category"],
+                "Description" : row["Description"]
+            })
+load_transactions("transactions.csv")
+print(transactions_list[0])
+print
+
+
